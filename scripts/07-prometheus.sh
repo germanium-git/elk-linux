93181d7406a75b193edd3e7a9ec7563e6c9f7b62
germanium-git <germanium-git@gmx.com>
2023-01-11 21:07:23 +0100

    prometheus

--- scripts/07-prometheus.sh
+++ scripts/07-prometheus.sh
@@ -48,12 +48,74 @@ ExecStart=/usr/local/bin/prometheus \
 WantedBy=multi-user.target
 EOF
 
+mv /etc/systemd/system/prometheus.service /etc/systemd/system/prometheus.service.backup
 
-chmod -R 775 /etc/prometheus/ /var/lib/prometheus/
+cat << EOF > /etc/systemd/system/prometheus.service
+# my global config                                                                                                                                                                                                                [12/846]
+global:
+  scrape_interval: 15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+  # scrape_timeout is set to the global default (10s).
+
+# Alertmanager configuration
+alerting:
+  alertmanagers:
+    - static_configs:
+        - targets:
+          # - alertmanager:9093
+
+# Load rules once and periodically evaluate them according to the global 'evaluation_interval'.
+rule_files:
+  # - "first_rules.yml"
+  # - "second_rules.yml"
+
+# A scrape configuration containing exactly one endpoint to scrape:
+# Here it's Prometheus itself.
+scrape_configs:
+  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  - job_name: "prometheus"
+
+    # metrics_path defaults to '/metrics'
+    # scheme defaults to 'http'.
+
+    static_configs:
+      - targets: ["localhost:9090"]
+  - job_name: "elasticsearch"
+    scrape_interval: 60s
+    scrape_timeout:  30s
+    metrics_path: "/metrics"
+    static_configs:
+    - targets: ["10.0.1.5:9114"]
+      labels:
+        service: elasticsearch
+    relabel_configs:
+    - source_labels: [__address__]
+      regex: '(.*)\:9108'
+      target_label:  'instance'
+      replacement:   '$1'
+    - source_labels: [__address__]
+      regex:         '.*\.(.*)\.lan.*'
+      target_label:  'environment'
+      replacement:   '$1'
+
+  - job_name: node
+    static_configs:
+    - targets: ["10.0.1.5:9100"]
+
+
+remote_write:
+- url: "https://prometheus-blocks-prod-us-central1.grafana.net/api/prom/push"
+  basic_auth:
+    username: "169071"
+    password: "eyJrIjo......................................1MTIxfQ=="
+
+EOF
 
 systemctl start prometheus
 sudo systemctl enable prometheus
 
 
 pubip=$(curl --silent ifconfig.me)
-echo "Try to access Prometheus on http://$pubip:5601"
+echo -e "Try to access Prometheus on http://$pubip:5601"
+
+echo -e "\nUpdate the password for remote_write: and restart the service"
